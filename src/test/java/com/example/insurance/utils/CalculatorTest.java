514438b54599f9aa880cf2cf507bdfb6c67870b0
Stanislav Matšel <stanislav.machel@gmail.com>
2020-08-05 04:45:25 +0300

    1 - Annual and monthly fee calculation

--- src/test/java/com/example/insurance/utils/CalculatorTest.java
+++ src/test/java/com/example/insurance/utils/CalculatorTest.java
@@ -7,13 +7,14 @@ public class CalculatorTest {
 
     @Test
     public void calculateAnnualFee() {
-        double result = Calculator.calculateAnnualFee(49687, 0.99, 1.1, 5);
-        Assert.assertEquals(2705.45715, result, 0.0001);
+        double result = Calculator.calculateAnnualFee(0.99, 49687, 0.08, 5, 1.1);
+        //0.99 * (49687 * 0.08 + 1.1 * 5) = 0.99 * (3974.96 + 5.5) = 3940.6554
+        Assert.assertEquals(3940.6554, result, 0.0001);
     }
 
     @Test
     public void calculateMonthlyFee() {
-        double result = Calculator.calculateMonthlyFee(49687, 0.99, 1.1, 5);
-        Assert.assertEquals(225.4547625, result, 0.0001);
+        double result = Calculator.calculateMonthlyFee(0.99, 49687, 0.08, 5, 1.1);
+        Assert.assertEquals(328.38795, result, 0.0001);
     }
 }
