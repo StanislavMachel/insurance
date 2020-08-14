04f76c0741633dd3b481a6054da16d28f548bb7d
Stanislav Matšel <stanislav.machel@gmail.com>
2020-08-14 10:29:15 +0300

    7 - Filter by indemnity risk

--- src/main/java/com/example/insurance/controllers/InsuranceCalcResultController.java
+++ src/main/java/com/example/insurance/controllers/InsuranceCalcResultController.java
@@ -7,7 +7,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequestMapping(InsuranceCalcResultController.URL)
 @RestController
@@ -20,7 +23,14 @@ public class InsuranceCalcResultController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<InsuranceCalcResult>> getAll(Pageable pageable) {
-        return ResponseEntity.ok(insuranceCalcResultRepository.findAll(pageable));
+    public ResponseEntity<Page<InsuranceCalcResult>> getAll(@RequestParam(required = false) List<String> includeParameterNames, Pageable pageable) {
+        Page<InsuranceCalcResult> page;
+        if (includeParameterNames != null) {
+            page = insuranceCalcResultRepository.findAllByIncludeParameterNamesListWithPagination(includeParameterNames, (long) includeParameterNames.size(), pageable);
+        } else {
+            page = insuranceCalcResultRepository.findAll(pageable);
+        }
+
+        return ResponseEntity.ok(page);
     }
 }
